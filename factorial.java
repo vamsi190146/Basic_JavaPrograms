ddca75e874389b8e6cc181f4f59f99cb4cfed3d1
vamsitadiparthi <tadiparthivamsi@gmail.com>
2024-07-16 18:27:27 +0530

    FactorialProgram

---
+++ factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FactorialRecursion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        long result = factorial(number);
+        System.out.println("The factorial of " + number + " is " + result);
+        
+        scanner.close();
+    }
+
+    public static long factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
