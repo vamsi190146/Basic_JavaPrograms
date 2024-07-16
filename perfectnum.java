fe2c269a525e726e233d48aebf8066f83d5b4e69
vamsitadiparthi <tadiparthivamsi@gmail.com>
2024-07-16 18:28:16 +0530

    PerfectNumber

---
+++ perfectnum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isPerfectNumber(number)) {
+            System.out.println(number + " is a perfect number.");
+        } else {
+            System.out.println(number + " is not a perfect number.");
+        }
+        
+        scanner.close();
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        int sum = 0;
+        
+        // Find all divisors and add them
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        
+        // Check if sum of divisors is equal to the number
+        return sum == number;
+    }
+}
