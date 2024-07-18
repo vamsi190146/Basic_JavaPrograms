dcfb8b4f774a0eb75bf5221d8a4d20f77d0491e3
vamsitadiparthi <tadiparthivamsi@gmail.com>
2024-07-18 21:32:14 +0530

    revStr

---
+++ revStr.java
@@ -0,0 +1,28 @@
+public class StringReversal {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String reversed = reverseString(original);
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        char[] charArray = str.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+        
+        while (left < right) {
+            // Swap characters at left and right pointers
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            
+            // Move pointers towards the center
+            left++;
+            right--;
+        }
+        
+        // Convert char array back to String
+        return new String(charArray);
+    }
+}
