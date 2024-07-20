782601cf1d1c7c719bb80527a72a17f468b7c972
vamsitadiparthi <tadiparthivamsi@gmail.com>
2024-07-20 20:55:49 +0530

    MissingValue

---
+++ MissingNum.java
@@ -0,0 +1,23 @@
+public class MissingNumber {
+    public static int findMissingNumber(int[] nums) {
+        int n = nums.length;  // Length of the array
+        
+        // Calculate the expected sum of numbers from 1 to n+1
+        int expectedSum = (n + 1) * (n + 2) / 2;
+        
+        // Calculate the actual sum of the given array
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+        
+        // The missing number is the difference between expected sum and actual sum
+        return expectedSum - actualSum;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 4, 5, 6};  // Example array with missing number 3
+        int missingNumber = findMissingNumber(nums);
+        System.out.println("The missing number is: " + missingNumber);
+    }
+}
