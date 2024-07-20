61d70f67a387489918935a833dd2a381b081db4a
vamsitadiparthi <tadiparthivamsi@gmail.com>
2024-07-20 20:55:17 +0530

    2dMatrix

---
+++ 2dMatrix.java
@@ -0,0 +1,39 @@
+public class MatrixSearch {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int row = 0;
+        int col = cols - 1;
+        
+        while (row < rows && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 4, 7, 11},
+            {2, 5, 8, 12},
+            {3, 6, 9, 16},
+            {10, 13, 14, 17}
+        };
+        
+        int target = 5;
+        boolean result = searchMatrix(matrix, target);
+        
+        System.out.println("Target found: " + result);
+    }
+}
