2fd4811d394d274f0f95d01b6bd0b46d9ca513af
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-07-24 21:10:26 +0530

    Add files via upload

---
+++ 2ndlar.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int print2largest(List<Integer> arr) {
+        if (arr == null || arr.size() < 2) {
+            return -1; 
+        }
+
+        int firstLargest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > firstLargest) {
+                secondLargest = firstLargest;
+                firstLargest = num;
+            } else if (num > secondLargest && num != firstLargest) {
+                secondLargest = num;
+            }
+        }
+
+        return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
+    }
+}
