5cbe81732d3462279a58ced73f3b71d041dde5bd
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-07-27 19:50:06 +0530

    Add files via upload

---
+++ BSearch.java
@@ -0,0 +1,30 @@
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            }
+            if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; // Key not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int key = 4;
+        int result = binarySearch(arr, key);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
