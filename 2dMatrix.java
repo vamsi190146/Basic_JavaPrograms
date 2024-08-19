c6884379b353222a4460d1ace841f18bf575bd84
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-08-19 23:14:47 +0530

    Update 2dMatrix.java

--- 2dMatrix.java
+++ 2dMatrix.java
@@ -8,7 +8,6 @@ public class MatrixSearch {
         
         int row = 0;
         int col = cols - 1;
-        
         while (row < rows && col >= 0) {
             if (matrix[row][col] == target) {
                 return true;
