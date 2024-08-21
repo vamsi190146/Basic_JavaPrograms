5edb60eb45e035cb748d0d93eb2bd966e1bb0815
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-08-21 22:08:18 +0530

    Update BSearch.java

--- BSearch.java
+++ BSearch.java
@@ -16,7 +16,6 @@ public class BinarySearch {
         }
         return -1; 
     }
-
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int key = 4;
