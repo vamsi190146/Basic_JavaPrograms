6eaa58e5573539042aa17c64bc06daeceef3c19c
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-08-22 20:26:21 +0530

    Update 2ndlar.java

--- 2ndlar.java
+++ 2ndlar.java
@@ -3,7 +3,6 @@ class Solution {
         if (arr == null || arr.size() < 2) {
             return -1; 
         }
-    
         int firstLargest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
         for (int num : arr) {
