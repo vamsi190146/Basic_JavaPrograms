800b923a4e4bb77d5611c4e09073dec7a0e9c671
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-08-23 09:48:08 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -10,15 +10,11 @@ public class AnagramChecker {
         }
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
-        
-        // Sort the character arrays
         Arrays.sort(arr1);
         Arrays.sort(arr2);
         
-        // Compare the sorted arrays
         return Arrays.equals(arr1, arr2);
-    }
-    
+    } 
     public static void main(String[] args) {
         String str1 = "listen";
         String str2 = "silent";
