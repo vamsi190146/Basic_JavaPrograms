c61e3acab5618158864b89a8d3cd9cee6b567b64
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-08-29 17:54:39 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -11,7 +11,6 @@ public class AnagramChecker {
         char[] arr2 = str2.toCharArray();
         Arrays.sort(arr1);
         Arrays.sort(arr2);
-        
         return Arrays.equals(arr1, arr2);
     } 
     public static void main(String[] args) {
