b5c3086e627678ae3e3d2b17ce9b856ad9700696
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-08-30 15:28:33 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -7,7 +7,6 @@ public class MergeSort {
             merge(arr, left, mid, right);
         }
     }
-
     public static void merge(int[] arr, int left, int mid, int right) {
         int n1 = mid - left + 1;
         int n2 = right - mid;
